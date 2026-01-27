00ca12458d6d519be62332704473ae67e0df23a3
kloworizer <esha.indra@gmail.com>
2026-01-27 12:05:55 +0700

    add klasifikasi tabel model

--- diamond_web/forms/__init__.py
+++ diamond_web/forms/__init__.py
@@ -1,2 +1,3 @@
 from .kategori_ilap import KategoriILAP
 from .jenis_tabel import JenisTabelForm
+from .klasifikasi_tabel import KlasifikasiTabelForm
--- diamond_web/models/__init__.py
+++ diamond_web/models/__init__.py
@@ -3,3 +3,4 @@ from .kategori_ilap import KategoriILAP
 from .ilap import ILAP
 from .jenis_tabel import JenisTabel
 from .kategori_wilayah import KategoriWilayah
+from .klasifikasi_tabel import KlasifikasiTabel
