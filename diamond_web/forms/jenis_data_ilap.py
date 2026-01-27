58d5cc40ed0c23ce0ef776cf6708b687a9c8e576
kloworizer <esha.indra@gmail.com>
2026-01-27 14:14:05 +0700

    add jenis data model

---
+++ diamond_web/forms/jenis_data_ilap.py
@@ -0,0 +1,28 @@
+from django import forms
+from ..models.jenis_data_ilap import JenisDataILAP
+
+class JenisDataILAPForm(forms.ModelForm):
+    class Meta:
+        model = JenisDataILAP
+        fields = [
+            'id_kategori_ilap',
+            'id_ilap',
+            'id_jenis_data',
+            'id_sub_jenis_data',
+            'nama_jenis_data',
+            'nama_sub_jenis_data',
+            'nama_tabel_I',
+            'nama_tabel_U',
+            'id_jenis_tabel',
+            'id_kategori_wilayah',
+            'id_klasifikasi_tabel'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add Bootstrap classes for better styling
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.Select):
+                field.widget.attrs['class'] = 'form-select'
+            else:
+                field.widget.attrs['class'] = 'form-control'
--- diamond_web/models/jenis_data_ilap.py
+++ diamond_web/models/jenis_data_ilap.py
@@ -1,14 +1,54 @@
 from django.db import models
+from .kategori_ilap import KategoriILAP
+from .ilap import ILAP
+from .jenis_tabel import JenisTabel
+from .kategori_wilayah import KategoriWilayah
+from .klasifikasi_tabel import KlasifikasiTabel
 
 class JenisDataILAP(models.Model):
-    id_jenis_data = models.CharField(max_length=7, primary_key=True, verbose_name="ID Jenis Data")
-    nama_jenis_data = models.CharField(max_length=2000, verbose_name="Nama Jenis Data")
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    id_kategori_ilap = models.ForeignKey(
+        KategoriILAP,
+        on_delete=models.CASCADE,
+        db_column="id_kategori_ilap",
+        verbose_name="Kategori ILAP"
+    )
+    id_ilap = models.ForeignKey(
+        ILAP,
+        on_delete=models.CASCADE,
+        db_column="id_ilap",
+        verbose_name="ILAP"
+    )
+    id_jenis_data = models.CharField(max_length=7, verbose_name="ID Jenis Data")
+    id_sub_jenis_data = models.CharField(max_length=9, verbose_name="ID Sub Jenis Data")
+    nama_jenis_data = models.CharField(max_length=255, verbose_name="Nama Jenis Data")
+    nama_sub_jenis_data = models.CharField(max_length=255, verbose_name="Nama Sub Jenis Data")
+    nama_tabel_I = models.CharField(max_length=255, verbose_name="Nama Tabel I")
+    nama_tabel_U = models.CharField(max_length=255, verbose_name="Nama Tabel U")
+    id_jenis_tabel = models.ForeignKey(
+        JenisTabel,
+        on_delete=models.CASCADE,
+        db_column="id_jenis_tabel",
+        verbose_name="Jenis Tabel"
+    )
+    id_kategori_wilayah = models.ForeignKey(
+        KategoriWilayah,
+        on_delete=models.CASCADE,
+        db_column="id_kategori_wilayah",
+        verbose_name="Kategori Wilayah"
+    )
+    id_klasifikasi_tabel = models.ForeignKey(
+        KlasifikasiTabel,
+        on_delete=models.CASCADE,
+        db_column="id_klasifikasi_tabel",
+        verbose_name="Klasifikasi Tabel"
+    )
 
     class Meta:
         verbose_name = "Jenis Data ILAP"
         verbose_name_plural = "Jenis Data ILAP"
         db_table = "jenis_data_ilap"
-        ordering = ["id_jenis_data"]
+        ordering = ["id"]
 
     def __str__(self):
         return f"{self.id_jenis_data} - {self.nama_jenis_data}"
