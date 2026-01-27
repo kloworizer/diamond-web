304c0176868c196f0b0dd5a14f35186fa483cc85
kloworizer <esha.indra@gmail.com>
2026-01-27 11:57:51 +0700

    add jenis tabel, kategori wilayah models

--- diamond_web/models/__init__.py
+++ diamond_web/models/__init__.py
@@ -1,3 +1,5 @@
 from .notification import Notification
 from .kategori_ilap import KategoriILAP
 from .ilap import ILAP
+from .jenis_tabel import JenisTabel
+from .kategori_wilayah import KategoriWilayah
