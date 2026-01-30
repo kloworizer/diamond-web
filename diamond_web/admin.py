4eea211604b9e3c8c6f59f49d18411563ac0c76b
Esha <esha.indra@gmail.com>
2026-01-30 19:11:35 +0700

    Removes unused model registrations from admin
    Simplifies admin configuration by only registering the notification model.
    Reduces unnecessary overhead and potential confusion from exposing unused models in the admin interface.

--- diamond_web/admin.py
+++ diamond_web/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from .models import Notification, TandaTerimaData, DetilTandaTerima, BackupData
+from .models import Notification
 
 admin.site.register(Notification)
-admin.site.register(TandaTerimaData)
-admin.site.register(DetilTandaTerima)
-admin.site.register(BackupData)
