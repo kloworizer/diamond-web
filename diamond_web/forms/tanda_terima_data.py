a9be7ec6120294bd23eee6e4d30ba7de0295d050
Esha <esha.indra@gmail.com>
2026-02-06 20:55:11 +0700

    feat: Enhance Tanda Terima Data management with active status, improved ticket selection, and AJAX handling

--- diamond_web/forms/tanda_terima_data.py
+++ diamond_web/forms/tanda_terima_data.py
@@ -1,8 +1,27 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
 from ..models.tanda_terima_data import TandaTerimaData
 from ..models.tiket import Tiket
+from ..models.ilap import ILAP
+from ..models.detil_tanda_terima import DetilTandaTerima
+
+
+class TiketCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
+    def __init__(self, *args, **kwargs):
+        self.disabled_ids = set(kwargs.pop('disabled_ids', []))
+        super().__init__(*args, **kwargs)
+
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option = super().create_option(name, value, label, selected, index, subindex=subindex, attrs=attrs)
+        try:
+            value_id = int(value)
+        except (TypeError, ValueError):
+            value_id = None
+        if value_id in self.disabled_ids:
+            option['attrs']['disabled'] = True
+        return option
 
 
 class TandaTerimaDataForm(forms.ModelForm):
@@ -28,6 +47,9 @@ class TandaTerimaDataForm(forms.ModelForm):
             if field_name != 'tiket_ids':
                 field.widget.attrs.update({'class': 'form-control'})
 
+        self._existing_tiket_ids = set()
+        self._disabled_tiket_ids = set(Tiket.objects.filter(status__gte=6).values_list('id', flat=True))
+
         # Auto-generate nomor_tanda_terima for new records
         if not self.instance.pk:
             self.fields['nomor_tanda_terima'].required = False
@@ -35,6 +57,8 @@ class TandaTerimaDataForm(forms.ModelForm):
             tanggal_input = self.data.get('tanggal_tanda_terima') if self.is_bound else None
             tanggal = parse_datetime(tanggal_input) if tanggal_input else None
             self.fields['nomor_tanda_terima'].initial = self._generate_nomor_tanda_terima(tanggal=tanggal)
+        else:
+            self.fields['nomor_tanda_terima'].disabled = True
         
         # If tiket_pk is provided, remove the tiket_ids field and pre-fill id_ilap
         if tiket_pk:
@@ -48,12 +72,65 @@ class TandaTerimaDataForm(forms.ModelForm):
             self.fields['id_ilap'].disabled = True
         # Pre-select tikets if editing
         elif self.instance.pk:
-            self.fields['tiket_ids'].initial = self.instance.detil_items.values_list('id_tiket', flat=True)
+            self._existing_tiket_ids = set(self.instance.detil_items.values_list('id_tiket_id', flat=True))
+            self.fields['tiket_ids'].initial = list(self._existing_tiket_ids)
+            self.fields['id_ilap'].disabled = True
+        else:
+            # Create flow: limit ILAP to those with available tikets
+            available_tiket_ids = Tiket.objects.filter(
+                status__lt=6
+            ).exclude(
+                id__in=DetilTandaTerima.objects.values_list('id_tiket_id', flat=True)
+            ).values_list('id', flat=True)
+            ilap_ids = ILAP.objects.filter(
+                jenisdatailap__periodejenisdata__tiket__id__in=available_tiket_ids
+            ).values_list('id', flat=True).distinct()
+            self.fields['id_ilap'].queryset = ILAP.objects.filter(id__in=ilap_ids)
+
+            # Bind tiket list to selected ILAP when form is bound
+            selected_ilap = self.data.get('id_ilap') if self.is_bound else None
+            if selected_ilap:
+                self.fields['tiket_ids'].queryset = Tiket.objects.filter(
+                    status__lt=6,
+                    id_periode_data__id_sub_jenis_data_ilap__id_ilap_id=selected_ilap
+                ).exclude(
+                    id__in=DetilTandaTerima.objects.values_list('id_tiket_id', flat=True)
+                )
+            else:
+                # Empty tiket list until ILAP selected
+                self.fields['tiket_ids'].queryset = Tiket.objects.none()
+
+        if 'tiket_ids' in self.fields:
+            self.fields['tiket_ids'].widget = TiketCheckboxSelectMultiple(disabled_ids=self._disabled_tiket_ids)
 
     def clean_id_ilap(self):
         if self.fields['id_ilap'].disabled:
             return self.fields['id_ilap'].initial
         return self.cleaned_data.get('id_ilap')
+
+    def clean_tiket_ids(self):
+        if 'tiket_ids' not in self.fields:
+            return self.cleaned_data.get('tiket_ids')
+
+        tiket_ids = set(self.cleaned_data.get('tiket_ids').values_list('id', flat=True))
+        if self.instance.pk:
+            tiket_ids |= self._disabled_tiket_ids & self._existing_tiket_ids
+
+        if not tiket_ids:
+            raise ValidationError('Minimal satu tiket harus dipilih.')
+
+        available_ids = set(
+            Tiket.objects.filter(status__lt=6)
+            .exclude(id__in=DetilTandaTerima.objects.values_list('id_tiket_id', flat=True))
+            .values_list('id', flat=True)
+        )
+        if self.instance.pk:
+            available_ids |= self._existing_tiket_ids
+
+        if not tiket_ids.issubset(available_ids | (self._disabled_tiket_ids & self._existing_tiket_ids)):
+            raise ValidationError('Beberapa tiket tidak tersedia untuk dipilih.')
+
+        return Tiket.objects.filter(id__in=tiket_ids)
 
     def save(self, commit=True):
         instance = super().save(commit=False)
