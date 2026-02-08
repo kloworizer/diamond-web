2b5712bff0dd998ec3e10c3849bf2fc028988c6a
Esha <esha.indra@gmail.com>
2026-02-08 12:16:41 +0700

    Adds task summary section for authenticated p3de users
    Enhances the home page by displaying a task summary for users in the p3de group after login. Improves user experience by surfacing relevant tasks directly on the dashboard.

--- diamond_web/views/home.py
+++ diamond_web/views/home.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.contrib.auth.models import Group
+from diamond_web.views.task_to_do import get_tiket_summary_for_user
 
 def home(request):
     context = {}
+    # expose whether the current user is in user_p3de group for templates
+    is_p3de = False
+    if request.user.is_authenticated:
+        is_p3de = request.user.groups.filter(name='user_p3de').exists()
+    context['is_p3de'] = is_p3de
+    # if p3de, compute task summary for inline display on home using helper
+    if is_p3de:
+        context['tiket_summary'] = get_tiket_summary_for_user(request.user)
     if settings.DEBUG:
         groups = Group.objects.filter(name__in=['user_p3de', 'user_pide', 'user_pmde']).prefetch_related('user_set')
         debug_groups = {}
