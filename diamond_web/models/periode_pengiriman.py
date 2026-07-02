65507164911b44d5ffb8a1bd3e0018c881b91db3
kloworizer <esha.indra@gmail.com>
2026-07-02 11:55:44 +0700

    fix: Remove uniqueness constraint from periode_penyampaian and add unique_together for periode_penyampaian and periode_penerimaan feat: Enhance periode options in rekam_tiket_form.html for Mingguan and 2 Mingguan types

--- diamond_web/models/periode_pengiriman.py
+++ diamond_web/models/periode_pengiriman.py
@@ -3,7 +3,7 @@ from .audit import AuditTrailModel
 
 class PeriodePengiriman(AuditTrailModel):
     id = models.AutoField(primary_key=True, verbose_name="ID")
-    periode_penyampaian = models.CharField(max_length=50, unique=True, verbose_name="Periode Penyampaian")
+    periode_penyampaian = models.CharField(max_length=50, verbose_name="Periode Penyampaian")
     periode_penerimaan = models.CharField(max_length=50, verbose_name="Periode Penerimaan")
 
     class Meta:
@@ -11,6 +11,7 @@ class PeriodePengiriman(AuditTrailModel):
         verbose_name_plural = "Periode Pengiriman"
         db_table = "periode_pengiriman"
         ordering = ["id"]
+        unique_together = ("periode_penyampaian", "periode_penerimaan")
 
     def __str__(self):
         return self.periode_penyampaian
